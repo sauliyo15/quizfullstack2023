3cec7c35deded08a1ad73980043a56e265acdc79
sauliyo15 <gc.saul.7@gmail.com>
2023-11-08 17:48:48 +0100

    Comentado routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
+//Importacion de modulos del sistems o instalados como paquetes npm
 var express = require('express');
+
+//Creacion del router
 var router = express.Router();
 
-/* GET home page. */
+
+//Instalacion de MWs router que atienden a las rutas indicadas: /
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  //Se elabora la respuesta con la renderizacion de la vista index.ejs pasando como parametro la variable title
+  res.render('index', { title: 'Hola soy Saúl' });
 });
 
+
+//Se define el modulo como exportable ya que se importará en el fichero de app.js entre otros
 module.exports = router;
