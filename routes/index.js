057c19a1f900379e8e2716f5e1cbe5bbc4ceaeb4
sauliyo15 <gc.saul.7@gmail.com>
2023-11-10 18:15:40 +0100

    Pagina de autor: imagenes, vista, rutas y enlaces

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
   //Se elabora la respuesta con la renderizacion de la vista index.ejs pasando como parametro la variable title
-  res.render('index', { title: 'Bienvenido al juego' });
+  res.render('index', { title: 'Juego' });
+});
+
+//Instalacion de MWs router que atienden a las rutas indicadas: /autor
+router.get('/autor', function(req, res, next) {
+
+  //Se elabora la respuesta con la renderizacion de la vista autor.ejs
+  res.render('autor');
 });
 
 
