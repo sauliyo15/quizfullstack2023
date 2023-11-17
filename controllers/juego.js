d2bf038b7b6359d7f947c8cc5f20f7543b49f2ca
sauliyo15 <gc.saul.7@gmail.com>
2023-11-17 15:02:02 +0100

    Añadido autoload de Juego y comentado

---
+++ controllers/juego.js
@@ -0,0 +1,24 @@
+//Se importa del directorio models el archivo index (nombre principal por defecto)
+const { models } = require("../models");
+
+
+/*Autoload el quiz asociado a :juegoId que precarga el juego indentificado en la tabla por el 
+:juegoId de la ruta y lo guarda en req.load.juego de esta forma los controladores que 
+tambien utilizan :juegoId pueden utilizarlo sin necesidad de cargarlo nuevamente*/
+exports.load = async (req, res, next, juegoId) => {
+  try {
+    //Se busca el juego a traves de su id en la base de datos
+    const juego = await models.Juego.findByPk(juegoId);
+    if (juego) {
+      //Si se encuentra el juego, se agrega al objeto 'load' en el objeto 'req' y se pasa al siguiente middleware o controlador  
+      req.load = { ...req.load, juego }; //Spread (clonacion)
+      next();
+    } else {
+      //Si no se encuentra el juego, se lanza un error  
+      throw new Error("No existe ningún juego con id: " + juegoId);
+    }
+  //Si hay un error durante la busqueda del juego, se pasa al siguiente middleware con el error  
+  } catch (error) {
+    next(error);
+  }
+};
